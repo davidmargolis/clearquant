041f808f2b97ff70a4efc1137eb4409fbf84bf98
davidmargolis <davidmargolis@outlook.com>
2020-05-08 03:35:31 -0400

    Added example circuits

--- README.md
+++ README.md
@@ -1,20 +1,13 @@
 # clearquant
 
-Quantum circuit solver
+Quantum Circuit Solver
 
 I did not get as much done as I wanted to but this application will draw circuits that you give in the form of QASM 2.0. You can you your console.log functionality to view the results of running the circuit. This was a big challenge for me, as someone who does not have a lot of website design experience but it was a great experience none the less. I would like to continue to improve this application to be more responsive and have more intuitive UI controls for editing the circuit.
 
-Currently the only way to edit the circuit is by pasting QASM compliant code into the circuit. Please take a look at https://github.com/Qiskit/openqasm/tree/master/examples. Just download some of those files and paste in the content to see it work.
+Currently the main way to edit the circuit is by pasting QASM compliant code into the circuit. Please take a look at https://github.com/Qiskit/openqasm/tree/master/examples. Just download some of those files and paste in the content to see it work. Many of these example files were added as "Example Circuits" as buttons that can be clicked on to load them immediately.
 
-# azure link
+# azure link - this is where the web app is deployed
 
 https://clearquant.azurewebsites.net/
-
-# worked for:
-
-https://raw.githubusercontent.com/Qiskit/openqasm/master/examples/generic/W-state.qasm
-https://raw.githubusercontent.com/Qiskit/openqasm/master/examples/generic/inverseqft1.qasm
-https://raw.githubusercontent.com/Qiskit/openqasm/master/examples/generic/inverseqft2.qasm
-https://raw.githubusercontent.com/Qiskit/openqasm/master/examples/generic/qft.qasm
 
 Please refresh the page if you encounter a bug.
--- src/app/quantum-circuit/quantum-circuit.component.ts
+++ src/app/quantum-circuit/quantum-circuit.component.ts
@@ -1,10 +1,26 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
-import { DOCUMENT } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { QasmValidatorDirective } from "./qasm-validator.directive";
+import { DownloaderService } from "./../downloader.service";
 import QuantumCircuit from "quantum-circuit";
 
+const EXAMPLE_CIRCUITS_LINK: string =
+  "https://raw.githubusercontent.com/Qiskit/openqasm/master/examples/";
+
+export class CircuitRef {
+  constructor(name: string, description: string, type: string) {
+    this.name = name;
+    this.description = description;
+    this.href = EXAMPLE_CIRCUITS_LINK + type + "/" + name + ".qasm";
+  }
+
+  name: string;
+  description: string;
+  href: string;
+}
+
 @Component({
+  providers: [DownloaderService],
   selector: "app-quantum-circuit",
   templateUrl: "./quantum-circuit.component.html",
   styleUrls: ["./quantum-circuit.component.scss"],
@@ -15,9 +31,64 @@ export class QuantumCircuitComponent implements OnInit {
     "",
     QasmValidatorDirective(this.circuit)
   );
-  @ViewChild("drawing") drawing: ElementRef;
+  exampleCircuits: CircuitRef[] = [
+    new CircuitRef("adder", "Adds two four-bit numbers.", "generic"),
+    new CircuitRef(
+      "bigadder",
+      "Quantum ripple-carry adder. 8-bit adder made out of 2 4-bit adders from adder.qasm.",
+      "generic"
+    ),
+    new CircuitRef(
+      "inverseqft1",
+      "Inverse quantum Fourier transform using 4 qubits.",
+      "generic"
+    ),
+    new CircuitRef(
+      "inverseqft2",
+      "Another version of the inverse quantum Fourier transform using 4 qubits.",
+      "generic"
+    ),
+    new CircuitRef(
+      "ipea_3_pi_8",
+      "4-bit Iterative Phase Estimation algorithm for phase 3pi/8 using two qubits.ss",
+      "generic"
+    ),
+    new CircuitRef(
+      "pea_3_pi_8",
+      "4-bit Phase Estimation algorithm for a phase 3pi/8 using 5 qubits.",
+      "generic"
+    ),
+    new CircuitRef(
+      "qec",
+      "Repetition code to correct quantum errors.",
+      "generic"
+    ),
+    new CircuitRef("qft", "Quantum Fourier transform on 4 qubits.", "generic"),
+    new CircuitRef("qpt", "Quantum Process Tomography example.", "generic"),
+    new CircuitRef(
+      "rb",
+      "Example of a single instance of two-qubits randomized benchmarking.",
+      "generic"
+    ),
+    new CircuitRef("teleport", "Quantum Teleportation example.", "generic"),
+    new CircuitRef(
+      "teleportv2",
+      "Quantum Teleportation example (one classical register).",
+      "generic"
+    ),
+    new CircuitRef(
+      "W-state",
+      "Generating a 3-qubit W-state using Toffoli gates.",
+      "generic"
+    ),
+    new CircuitRef(
+      "Deutsch_Algorithm",
+      "A two-qubit example of Deutsch to determine whether a function is constant (in which case the algorithm returns 0) or balanced (returns 1). In this example the algorithm looks at the function f(x) = x, which is balanced.",
+      "ibmqx2"
+    ),
+  ];
 
-  constructor() {}
+  constructor(private downloaderService: DownloaderService) {}
 
   ngOnInit() {
     this.textInput.valueChanges.subscribe((value) => {
@@ -27,6 +98,12 @@ export class QuantumCircuitComponent implements OnInit {
       }
     });
     this.updateExpressionBar();
+  }
+
+  onExampleCircuitClick(href: string) {
+    this.downloaderService
+      .getTextFile(href)
+      .subscribe((results) => this.textInput.setValue(results));
   }
 
   update() {
